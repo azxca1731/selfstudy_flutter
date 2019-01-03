4331c9ca00b42a4a7c978bbe1f958273d7551067
이정훈 <ijeonghun@ijeonghun-ui-MacBook-Pro.local>
2019-01-03 21:46:27 +0900

    first commit

---
+++ first_step/lib/main.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+import 'package:english_words/english_words.dart';
+
+void main() => runApp(myApp());
+
+class myApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    final wordPair = WordPair.random();
+    return MaterialApp(
+      title: 'Welcome to Flutter',
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('Welcome to Flutter'),
+        ),
+        body: Center(
+          child: Text(wordPair.asPascalCase),
+        ),
+      ),
+    );
+  }
+}
