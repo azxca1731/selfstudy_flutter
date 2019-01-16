cf293c4689223db9fd0a33361409adc53f2acdab
azxca1731 <azxca1731@gmail.com>
2019-01-16 14:23:28 +0900

    using map

--- udemy_first/lib/main.dart
+++ udemy_first/lib/main.dart
@@ -2,33 +2,48 @@ import 'package:flutter/material.dart';
 
 void main() => runApp(MyApp());
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() {
+    return _MyAppState();
+  }
+}
+
+class _MyAppState extends State<MyApp> {
+  List<String> _product = ["Food Paradise!!", "More Happier More Fat"];
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: Scaffold(
-          appBar: AppBar(
-            title: Text('NewBie App'),
+        home: Scaffold(
+      appBar: AppBar(
+        title: Text('NewBie App'),
+      ),
+      body: Column(children: <Widget>[
+        Container(
+          margin: EdgeInsets.all(10.0),
+          child: RaisedButton(
+            onPressed: () {
+              setState(() {
+                _product.add('Advanced Food Paradise');
+                print(_product);
+              });
+            },
+            child: Text('Add Product'),
           ),
-          body: Column(
-            children: <Widget>[
-              Container(
-                margin: EdgeInsets.all(10.0),
-                child: RaisedButton(
-                  onPressed: () {},
-                  child: Text('Add Product'),
-                ),
-              ),
-              Card(
-                child: Column(
-                  children: <Widget>[
-                    Image.asset('assets/food.jpg'),
-                    Text('Food Paradise!')
-                  ],
-                ),
-              ),
-            ],
-          )),
-    );
+        ),
+        Column(
+          children: _product
+              .map((product) => Card(
+                    child: Column(
+                      children: <Widget>[
+                        Image.asset('assets/food.jpg'),
+                        Text(product)
+                      ],
+                    ),
+                  ))
+              .toList(),
+        )
+      ]),
+    ));
   }
 }
