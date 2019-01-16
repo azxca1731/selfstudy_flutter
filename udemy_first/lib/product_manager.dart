dbfccc8b7b79518994b52c3c98382ee9d07d5c65
azxca1731 <azxca1731@gmail.com>
2019-01-16 16:57:59 +0900

    상태 끌어올리기

--- udemy_first/lib/product_manager.dart
+++ udemy_first/lib/product_manager.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import './product.dart';
+import './product_control.dart';
 
 class ProductManager extends StatefulWidget {
   final String initialValue;
@@ -7,37 +8,29 @@ class ProductManager extends StatefulWidget {
   ProductManager({this.initialValue = 'no needs'});
 
   @override
-    State<StatefulWidget> createState() {     
-      return _ProductManagerState();
-    }
+  State<StatefulWidget> createState() {
+    return _ProductManagerState();
+  }
 }
 
 class _ProductManagerState extends State<ProductManager> {
   List<String> _product = [];
 
   @override
-    void initState() {
-      super.initState();
-      _product.add(widget.initialValue);
-    }
+  void initState() {
+    super.initState();
+    _product.add(widget.initialValue);
+  }
+
+  void addProduct(String productName) {
+    setState(() {
+      _product.add(productName);
+    });
+  }
 
   @override
-    Widget build(BuildContext context) {
-      return Column(children: <Widget>[
-        Container(
-          margin: EdgeInsets.all(10.0),
-          child: RaisedButton(
-            color: Theme.of(context).primaryColor,
-            onPressed: () {
-              setState(() {
-                _product.add('Advanced Food Paradise');
-                print(_product);
-              });
-            },
-            child: Text('Add Product'),
-          ),
-        ),
-        Products(_product)
-      ]);
-    }
+  Widget build(BuildContext context) {
+    return Column(
+        children: <Widget>[ProductControl(addProduct), Products(_product)]);
+  }
 }
