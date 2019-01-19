d8926dfaf5df291662fb6983e992409bf81178b2
azxca1731 <azxca1731@gmail.com>
2019-01-19 18:05:56 +0900

    드로워(햄버거) 추가

--- udemy_first/lib/pages/product_list.dart
+++ udemy_first/lib/pages/product_list.dart
@@ -4,12 +4,26 @@ import '../product_manager.dart';
 
 class ProductList extends StatelessWidget {
   @override
-    Widget build(BuildContext context) {
-      return Scaffold(
+  Widget build(BuildContext context) {
+    return Scaffold(
+      drawer: Drawer(
+        child: Column(
+          children: <Widget>[
+            AppBar(
+              automaticallyImplyLeading: false,
+              title: Text('Choose'),
+            ),
+            ListTile(
+              title: Text('Products Management'),
+              onTap: () {},
+            ),
+          ],
+        ),
+      ),
       appBar: AppBar(
         title: Text('NewBie App'),
       ),
       body: ProductManager(),
     );
-    }
+  }
 }
