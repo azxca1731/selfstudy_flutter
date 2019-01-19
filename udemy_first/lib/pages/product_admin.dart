940f9886580935debccda7753e04134cd0a10001
azxca1731 <azxca1731@gmail.com>
2019-01-19 21:57:24 +0900

    Create Tabs & Controller

--- udemy_first/lib/pages/product_admin.dart
+++ udemy_first/lib/pages/product_admin.dart
@@ -1,36 +1,55 @@
 import 'package:flutter/material.dart';
 
 import './product_list.dart';
+import './create_product.dart';
+import './my_product.dart';
 
 class ProductAdmin extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      drawer: Drawer(
-        child: Column(
-          children: <Widget>[
-            AppBar(
-              automaticallyImplyLeading: false,
-              title: Text('Choose'),
+    return DefaultTabController(
+      length: 2,
+      child: Scaffold(
+          drawer: Drawer(
+            child: Column(
+              children: <Widget>[
+                AppBar(
+                  automaticallyImplyLeading: false,
+                  title: Text('Admin'),
+                ),
+                ListTile(
+                  title: Text('Products List'),
+                  onTap: () => Navigator.pushReplacement(
+                        context,
+                        MaterialPageRoute(
+                          builder: (BuildContext context) => ProductList(),
+                        ),
+                      ),
+                ),
+              ],
             ),
-            ListTile(
-              title: Text('Products Management'),
-              onTap: () => Navigator.pushReplacement(
-                    context,
-                    MaterialPageRoute(
-                      builder: (BuildContext context) => ProductList(),
-                    ),
-                  ),
+          ),
+          appBar: AppBar(
+            title: Text('Admin'),
+            bottom: TabBar(
+              tabs: <Widget>[
+                Tab(
+                  icon: Icon(Icons.create),
+                  text: 'Create Product',
+                ),
+                Tab(
+                  icon: Icon(Icons.list),
+                  text: 'My Product',
+                ),
+              ],
             ),
-          ],
-        ),
-      ),
-      appBar: AppBar(
-        title: Text('Admin'),
-      ),
-      body: Center(
-        child: Text('Put on Item on it'),
-      ),
+          ),
+          body: TabBarView(
+            children: <Widget>[
+              MyProduct(),
+              ProductCreate(),
+            ],
+          )),
     );
   }
 }
