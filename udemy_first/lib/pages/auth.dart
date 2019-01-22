62ddc0899ad1417a8d81c6d10bba2847aa183352
azxca1731 <azxca1731@gmail.com>
2019-01-22 20:53:56 +0900

    auth validate 추가

--- udemy_first/lib/pages/auth.dart
+++ udemy_first/lib/pages/auth.dart
@@ -12,9 +12,12 @@ class AuthPage extends StatefulWidget {
 }
 
 class _AuthPageState extends State<AuthPage> {
-  String _idValue;
-  String _passwordValue;
-  bool _acceptTerms = false;
+  final Map<String, dynamic> _authFormValue = {
+    'email': null,
+    'password': null,
+    'acceptTerms': false
+  };
+  final GlobalKey<FormState> _authKey = GlobalKey<FormState>();
 
   BoxDecoration _buildBackgroundImage() {
     return BoxDecoration(
@@ -28,42 +31,65 @@ class _AuthPageState extends State<AuthPage> {
   }
 
   Widget _buildIDTextField() {
-    return TextField(
+    return TextFormField(
       keyboardType: TextInputType.emailAddress,
       decoration: InputDecoration(
         labelText: 'ID',
         filled: true,
         fillColor: Colors.white,
       ),
-      onChanged: (String value) {
-        setState(() {
-          _idValue = value;
-        });
+      validator: (String value) {
+        if (value.isEmpty ||
+            !RegExp(r"[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?")
+                .hasMatch(value)) {
+          return 'Email cannot be Empty or value is not type of Email';
+        }
+      },
+      onSaved: (String value) {
+        _authFormValue['email'] = value;
       },
     );
   }
 
   Widget _buildPasswordTextField() {
-    return TextField(
+    return TextFormField(
       obscureText: true,
       decoration: InputDecoration(
         labelText: 'Password',
         filled: true,
         fillColor: Colors.white,
       ),
-      onChanged: (String value) {
-        setState(() {
-          _passwordValue = value;
-        });
+      validator: (String value) {
+        if (value.isEmpty || value.length < 6) {
+          return 'Password cannot be Empty or should be 5+ character long';
+        }
+      },
+      onSaved: (String value) {
+        _authFormValue['password'] = value;
       },
     );
   }
 
+  Widget _buildAcceptTerms() {
+    return SwitchListTile(
+      value: _authFormValue['acceptTerms'],
+      onChanged: (bool value) {
+        setState(() {
+          _authFormValue['acceptTerms'] = value;
+        });
+      },
+      title: Text('Accept Terms'),
+    );
+  }
+
   void _submitForm() {
-    if (_idValue.length == 0 || _passwordValue.length == 0) {
-    } else {
-      Navigator.pushReplacementNamed(context, '/products');
+    if (!_authKey.currentState.validate() || !_authFormValue['acceptTerms']) {
+      print(_authKey);
+      return;
     }
+    _authKey.currentState.save();
+    print(_authFormValue);
+    Navigator.pushReplacementNamed(context, '/products');
   }
 
   @override
@@ -81,32 +107,27 @@ class _AuthPageState extends State<AuthPage> {
           child: SingleChildScrollView(
             child: Container(
               width: targetWidth,
-              child: Column(
-                children: <Widget>[
-                  _buildIDTextField(),
-                  SizedBox(
-                    height: 10.0,
-                  ),
-                  _buildPasswordTextField(),
-                  SwitchListTile(
-                    value: _acceptTerms,
-                    onChanged: (bool value) {
-                      setState(() {
-                        _acceptTerms = value;
-                      });
-                    },
-                    title: Text('Accept Terms'),
-                  ),
-                  SizedBox(
-                    height: 10.0,
-                  ),
-                  RaisedButton(
-                    child: Text('Login'),
-                    color: Theme.of(context).primaryColor,
-                    textColor: Colors.white,
-                    onPressed: _submitForm,
-                  ),
-                ],
+              child: Form(
+                key: _authKey,
+                child: Column(
+                  children: <Widget>[
+                    _buildIDTextField(),
+                    SizedBox(
+                      height: 10.0,
+                    ),
+                    _buildPasswordTextField(),
+                    _buildAcceptTerms(),
+                    SizedBox(
+                      height: 10.0,
+                    ),
+                    RaisedButton(
+                      child: Text('Login'),
+                      color: Theme.of(context).primaryColor,
+                      textColor: Colors.white,
+                      onPressed: _submitForm,
+                    ),
+                  ],
+                ),
               ),
             ),
           ),
